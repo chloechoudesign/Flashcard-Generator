8b4bdb7344c619b1b308437cd0822df25afd1d60
Your NameChloe <chloechoudesign@gmail.com>
2017-09-07 14:08:11 -0700

    cloze card complete

--- cloze-card.js
+++ cloze-card.js
@@ -1,8 +1,39 @@
 module.exports = ClozeCard;
 
 function ClozeCard(text, cloze){
-  this.cloze = cloze;
-  this.partial = partial;
-  this.fullText = fullText;
+ this.cloze = cloze;
+ this.text = text;
+
+ this.printCloze = function(){
+    console.log(this.cloze);
+ }
+
+ this.printText = function(){
+    console.log(this.text);
+ }
+
+ this.printPartial = function(){
+    var fullText = this.text;
+    var partial = fullText.replace(this.cloze, '...');
+    // console.log(this.cloze);
+    // console.log(this.text);
+    console.log(partial);
+ }
 }
+
+var firstPresidentCloze = new ClozeCard(
+  "George Washington was the first president of the United States.",
+  "George Washington");
+
+// console.log(firstPresidentCloze.cloze);
+// console.log(firstPresidentCloze.text);
+// console.log(firstPresidentCloze.printPartial());
+
+firstPresidentCloze.printCloze();
+firstPresidentCloze.printText();
+firstPresidentCloze.printPartial();
+
+
+
+
 
